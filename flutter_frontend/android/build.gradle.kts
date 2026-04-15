2aa064dc738fe2d83e1e80280d2a52e582be6261
clyde.anderson <clyde.anderson@dynatrace.com>
2026-04-14 20:03:41 -0700

    removed dynatrace artifacts in the flutter configs

--- flutter_frontend/android/build.gradle.kts
+++ flutter_frontend/android/build.gradle.kts
@@ -1,18 +1,4 @@
 plugins {
-    id("com.dynatrace.instrumentation") version "8.333.1.1006"
-}
-extra["dynatrace.instrumentationFlavor"] = "flutter"
-dynatrace {
-    configurations {
-        create("defaultConfig") {
-            autoStart{
-                applicationId("40a4f0a1-14a1-4555-b434-fa95b0935988")
-                beaconUrl("https://bf40364blh.bf-sprint.dynatracelabs.com/mbeacon")
-            }
-            agentBehavior.startupLoadBalancing(true)
-            agentBehavior.startupWithGrailEnabled(true)
-        }
-    }
 }
 allprojects {
     repositories {
